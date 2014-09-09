877aedf6870882ef410ae4005e6403a749df907c
Barbaroti <alvaroy@outlook.com>
2014-09-09 14:29:23 -0500

    PM_Master done

---
+++ src/com/alvaroy/promediouninorte/PM_Subject.java
@@ -0,0 +1,18 @@
+package com.alvaroy.promediouninorte;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PM_Subject extends Fragment {
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			ViewGroup container, Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.pm_subject, container, false);
+		return rootView;
+	}
+
+}
