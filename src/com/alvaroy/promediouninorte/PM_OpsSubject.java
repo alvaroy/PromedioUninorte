9dbf1479039d5b078b133a2936135cd5941f678e
Barbaroti <alvaroy@outlook.com>
2014-09-11 18:54:49 -0500

    PM_Subject done

---
+++ src/com/alvaroy/promediouninorte/PM_OpsSubject.java
@@ -0,0 +1,21 @@
+package com.alvaroy.promediouninorte;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class PM_OpsSubject extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			ViewGroup container, Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.pm_opssubject, container, false);
+		return rootView;
+	}
+	
+	
+
+}
--- src/com/alvaroy/promediouninorte/PM_Subject.java
+++ src/com/alvaroy/promediouninorte/PM_Subject.java
@@ -2,7 +2,6 @@ package com.alvaroy.promediouninorte;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import com.alvaroy.promediouninorte.database.DatabaseHelper;
 import com.alvaroy.promediouninorte.database.Student;
 import com.alvaroy.promediouninorte.database.StudentSubject;
@@ -11,15 +10,20 @@ import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.j256.ormlite.dao.RuntimeExceptionDao;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class PM_Subject extends Fragment {
 	
 	ArrayAdapter<String> adapter;
+	ArrayList<Integer> ids = new ArrayList<Integer>();
 	ListView subject;
 	View rootView;
 	
@@ -32,14 +36,30 @@ public class PM_Subject extends Fragment {
 		
 		//Auto-load		
 		ArrayList<String> list = makeList();
-		adapter = new ArrayAdapter<String>(rootView.getContext(), android.R.layout.simple_list_item_1, list);
+		adapter = new ArrayAdapter<String>(rootView.getContext(), android.R.layout.simple_list_item_1, list);		
 		subject.setAdapter(adapter);
+		
+		//ListView method to change fragment
+		subject.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View v, int position,
+					long id) {
+				Bundle args = getArguments();
+				args.putInt("ID", ids.get(position));				
+				PM_OpsSubject fragment = new PM_OpsSubject();
+				fragment.setArguments(args);
+				FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+				ft.replace(R.id.main, fragment).addToBackStack("PM_Subject").commit();
+			}
+		});
 		
 		return rootView;
 	}
 	
 	private ArrayList<String> makeList() {
 		ArrayList<String> list = new ArrayList<String>();
+		ids.clear();
 		DatabaseHelper helper = OpenHelperManager.getHelper(rootView.getContext(), DatabaseHelper.class);
 		RuntimeExceptionDao<Student, Integer> studentDAO = helper.getStudentRuntimeDAO();
 		RuntimeExceptionDao<Subject, Integer> subjectDAO = helper.getSubjectRuntimeDAO();
@@ -47,7 +67,9 @@ public class PM_Subject extends Fragment {
 		List<StudentSubject> names = stusubDAO.queryForEq("student_id", studentDAO.queryForEq("user", getArguments().getString("Username")).get(0).getId());
 		for (StudentSubject stusub : names) {
 			list.add(subjectDAO.queryForId(stusub.getSubject().getId()).getName());
+			ids.add(stusub.getSubject().getId());			
 		}
+		OpenHelperManager.releaseHelper();
 		return list;
 	}
 
