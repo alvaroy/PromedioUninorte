7e90f9a0430f139af280e7ff8b505a0a4d3277e3
Barbaroti <alvaroy@outlook.com>
2014-09-13 04:20:11 -0500

    Change action bar to red

--- src/com/alvaroy/promediouninorte/MainActivity.java
+++ src/com/alvaroy/promediouninorte/MainActivity.java
@@ -1,5 +1,7 @@
 package com.alvaroy.promediouninorte;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
@@ -8,6 +10,7 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.RED));
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.main, new Master()).commit();
